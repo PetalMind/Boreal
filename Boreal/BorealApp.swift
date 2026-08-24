bc04b3a2dbbabbb37c487c5d968e76e1aee22571
Dominik <dominikjaros99@icloud.com>
2026-08-24 12:23:04 +0200

    Initial Commit

---
+++ Boreal/BorealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BorealApp.swift
+//  Boreal
+//
+//  Created by Dominik on 24/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BorealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BorealTests/BorealTests.swift
@@ -0,0 +1,19 @@
+//
+//  BorealTests.swift
+//  BorealTests
+//
+//  Created by Dominik on 24/08/2026.
+//
+
+import Testing
+@testable import Boreal
+
+struct BorealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
